9504c13e33b051425a9ba01eebdbb47d1393eee8
nosherwan537 <tahirnosherwan@gmail.com>
2024-11-25 14:42:39 +0500

    Initial commit , alongwith lexicon and preprocessing implementation

---
+++ backend/lexicon.py
@@ -0,0 +1,23 @@
+import pickle
+
+with open('cleaned_texts.pkl','rb') as f:
+    cleaned_texts=pickle.load(f)
+
+def build_lexicon(texts):
+    lexicon=set()
+    word_id=0
+    for text in cleaned_texts:
+        for word in text:
+            if word not in lexicon:
+                lexicon.add(word)
+                word_id+=1
+    return lexicon
+
+lexicon=build_lexicon(cleaned_texts)
+print("Lexicon size:",len(lexicon))
+
+
+with open('lexicon.pkl','wb') as f:
+    pickle.dump(lexicon,f)
+
+print("Lexicon built!")
---
+++ backend/preprocessing.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import re
+import pickle
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('punkt_tab')
+
+df=pd.read_csv('../medium_articles.csv')
+texts=df['text'].fillna("").tolist()
+
+def clean_text(text):
+    text =re.sub(r'[^\w\s]','',text)
+    tokens=word_tokenize(text.lower())
+    stop_words=set(stopwords.words('english'))
+    return [i for i in tokens if i not in stop_words]
+
+cleaned_texts=[clean_text(text) for text in texts]
+
+with open('cleaned_texts.pkl','wb') as f:
+    pickle.dump(cleaned_texts,f)
+
+print("Preprocessing done!")
