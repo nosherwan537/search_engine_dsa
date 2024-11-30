7e3e2cf35e13ca5b62878fd60e6f75e8f409df31
Daud Shafi Khan <shafidaud111@gmail.com>
2024-11-30 20:57:40 +0500

    Minor preprocessing change

--- backend/preprocessing.py
+++ backend/preprocessing.py
@@ -10,21 +10,16 @@ nltk.download('punkt_tab')
 
 df=pd.read_csv(r'C:\Users\D E L L\Documents\search_engine\medium_articles.csv')
 texts=df['text'].fillna("").tolist()
+stop_words = set(stopwords.words('english'))
 
-def clean_text(docID,text):
+def clean_text(text):
     text =re.sub(r'[^\w\s]','',text)
     tokens=word_tokenize(text.lower())
-    stop_words=set(stopwords.words('english'))
-    pos_word = []
-    for pos,word in enumerate(tokens):
-        if word not in stop_words:
-            pos_word.append((pos,word))
+    return [i for i in tokens if i not in stop_words]
 
-    return (docID,pos_word)
+cleaned_texts=[clean_text(text) for docID,text in enumerate(texts)]
 
-cleaned_texts=[clean_text(docID,pos_word) for docID,pos_word in enumerate(texts)]
-
-with open('cleaned_texts.pkl','wb') as f:
+with open('../cleaned_texts.pkl','wb') as f:
     pickle.dump(cleaned_texts,f)
 
 print("Preprocessing done!")
