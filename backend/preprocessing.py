bcac8fd8ee088a830c5e51b0f70d70c8bc274820
Daud Shafi Khan <shafidaud111@gmail.com>
2024-12-01 16:57:15 +0500

    preprocessing

--- backend/preprocessing.py
+++ backend/preprocessing.py
@@ -12,12 +12,13 @@ df=pd.read_csv(r'C:\Users\D E L L\Documents\search_engine\medium_articles.csv')
 texts=df['text'].fillna("").tolist()
 stop_words = set(stopwords.words('english'))
 
-def clean_text(text):
+def clean_text(docID,text):
     text =re.sub(r'[^\w\s]','',text)
     tokens=word_tokenize(text.lower())
-    return [i for i in tokens if i not in stop_words]
+    cleaned_tokens = [i for i in tokens if i not in stop_words]
+    return (docID,cleaned_tokens)
 
-cleaned_texts=[clean_text(text) for docID,text in enumerate(texts)]
+cleaned_texts=[clean_text(docID,text) for docID,text in enumerate(texts)]
 
 with open('../cleaned_texts.pkl','wb') as f:
     pickle.dump(cleaned_texts,f)
